1ca595c4e776f3ca42cdafccec9d69d9504ad169
Shubhamkar Sambhaji Patil <149138866+Shubhamkar20@users.noreply.github.com>
2024-03-16 06:00:32 +0000

    change

--- assets/script.js
+++ assets/script.js
@@ -1,85 +1,61 @@
 
+const apiKey = '14cb86ac492a406f81006f55ee58f978';
+const url = 'https://newsapi.org/v2/everything?q=';
 
-// apikey = 'db7e613999f8307172b01196cfbf1c61';
-// url = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=' + apikey;
-
-// fetch(url)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     articles = data.articles;
-
-//     for (i = 0; i < articles.length; i++) {
-//       // articles[i].title
-//       console.log("Title: " + articles[i]['title']);
-//       // articles[i].description
-//       console.log("Description: " + articles[i]['description']);
-//       // You can replace {property} below with any of the article properties returned by the API.
-//       // articles[i].{property}
-//       // console.log(articles[i]['{property}']);
-
-//       // Delete this line to display all the articles returned by the request. Currently only the first article is displayed.
-//       break;
-//     }
-//   });
-const apiKey = 'db7e613999f8307172b01196cfbf1c61';
-const url = 'https://gnews.io/api/v4/search?q=';
-
-// window.addEventListener('load', ()=> getNews("India"));
+window.addEventListener('load', ()=> getNews("India"));
 
 async function getNews(query) {
     const response = await fetch(`${url}${query}&apiKey=${apiKey}`);
     const data = await response.json();
     // bindArticles(data.articles)
-    console.log(data.articles)
+    console.log(data)
 }
-getNews('India')
+// getNews('India')
 
-function bindArticles(articles) {
-    const rowMain = document.getElementById('row');
-    const template = document.getElementById('template');
+// function bindArticles(articles) {
+//     const rowMain = document.getElementById('row');
+//     const template = document.getElementById('template');
 
-    rowMain.innerHTML = '';
-    articles.forEach((article) => {
-        if (!article.urlToImage) return;
+//     rowMain.innerHTML = '';
+//     articles.forEach((article) => {
+//         if (!article.urlToImage) return;
 
-        const articleClone = template.content.cloneNode(true);
-        addData(articleClone, article);
+//         const articleClone = template.content.cloneNode(true);
+//         addData(articleClone, article);
 
-        rowMain.appendChild(articleClone);
-    });
-}
+//         rowMain.appendChild(articleClone);
+//     });
+// }
 
-function addData(articleClone, article){
-    const newsImage = articleClone.getElementById('newsImg');
-    const cardTitle = articleClone.getElementById('card-title');
-    const cardDes = articleClone.getElementById('card-description');
-    const newsSource = articleClone.getElementById('source')
+// function addData(articleClone, article){
+//     const newsImage = articleClone.getElementById('newsImg');
+//     const cardTitle = articleClone.getElementById('card-title');
+//     const cardDes = articleClone.getElementById('card-description');
+//     const newsSource = articleClone.getElementById('source')
 
-    newsImage.src = article.urlToImage;
-    cardTitle.innerHTML = article.title;
-    cardDes.innerHTML = article.description;
+//     newsImage.src = article.urlToImage;
+//     cardTitle.innerHTML = article.title;
+//     cardDes.innerHTML = article.description;
     
-    const date = new Date(article.publishedAt).toLocaleString()
-    newsSource.innerHTML = date;
+//     const date = new Date(article.publishedAt).toLocaleString()
+//     newsSource.innerHTML = date;
 
-    articleClone.firstElementChild.addEventListener('click', ()=>{
-        window.open(article.url, '_blank');
-    });
-
-
-}
+//     articleClone.firstElementChild.addEventListener('click', ()=>{
+//         window.open(article.url, '_blank');
+//     });
 
 
-function searchQuery(){
-    event.preventDefault();
-    const query = document.getElementById('searchInput').value;
-    if(!query){
-        return;
-    }
-    getNews(query);
-    console.log(query)
+// }
 
-}
+
+// function searchQuery(){
+//     event.preventDefault();
+//     const query = document.getElementById('searchInput').value;
+//     if(!query){
+//         return;
+//     }
+//     getNews(query);
+//     console.log(query)
+
+// }
 
